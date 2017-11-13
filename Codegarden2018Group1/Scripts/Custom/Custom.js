260823af94e41d194d43b332a2372b6ecfaccccc
LobergDesign <jeanloberg@hotmail.dk>
2017-11-13 21:50:46 +0100

    Playing with menu transitions

--- Codegarden2018Group1/Scripts/Custom/Custom.js
+++ Codegarden2018Group1/Scripts/Custom/Custom.js
@@ -3,4 +3,5 @@
 $("[data-menu-toggle]").click(function (){
     $(this).toggleClass("active");
     $("[data-menu-content]").toggleClass("active");
+$("[data-main-wrapper]").toggleClass("active");
 });
