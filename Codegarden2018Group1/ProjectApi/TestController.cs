75489e5506a0295cd2ecf0cca88a3a376daa819f
LobergDesign <jeanloberg@hotmail.dk>
2017-12-04 09:33:06 +0100

    fixed slider

---
+++ Codegarden2018Group1/ProjectApi/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+
+namespace Codegarden2018Group1.ProjectApi
+{
+    public class TestController : UmbracoApiController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
